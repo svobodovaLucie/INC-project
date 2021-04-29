f44be55e1c8641366214b0f2d0c5fec88f5cf66e
svobodovaLucie <xsvobo1x@stud.fit.vutbr.cz>
2021-04-29 13:10:36 +0200

    D synchro added

--- uart.vhd
+++ uart.vhd
@@ -1,5 +1,5 @@
 -- uart.vhd: UART controller - receiving part
--- Author(s): Lucie Svobodová¡, xsvobo1x
+-- Author(s): Lucie Svobodová, xsvobo1x
 --
 library ieee;
 use ieee.std_logic_1164.all;
@@ -21,69 +21,96 @@ end UART_RX;
 architecture behavioral of UART_RX is
 signal clk_cnt    : std_logic_vector(4 downto 0);   -- citac hodinovych signalu
 signal bit_cnt    : std_logic_vector(3 downto 0);   -- citac bitu (pouzity pri DATA_READ)
-signal clk_cnt_en : std_logic;                      -- signalizace, zda je aktivovan citac hodinoveho signalu
-signal dread      : std_logic;                      -- signalizace, zda je aktivovan rezim cteni dat
-signal D_VLD      : std_logic;                      -- signalizace, zda je nastaven DOUT_VLD
-signal stop_bit_w : std_logic;                      -- signalizace, ze cekame na stop bit
-signal stop_bit_en: std_logic;
-signal dvalid     : std_logic;
+signal clk_cnt_en : std_logic;                      -- indikace, zda je aktivovan citac hodinoveho signalu
+signal dread      : std_logic;                      -- indikace, zda je aktivovan rezim cteni dat
+signal D_VLD      : std_logic;                      -- indikace, zda je nastaven DOUT_VLD
+signal stop_bit_w : std_logic;                      -- indikace, ze cekame na stop bit
+signal stop_bit_en: std_logic;                      -- indikace, ze byl stop bit detekovan
+signal dvalid     : std_logic;                      -- indikace, ze dvalid ma byt nastaven
+signal rst_syn0 : std_logic;                -- vystup z D klopneho obvodu po prvni urovni synchronizace signalu RST
+signal rst_syn : std_logic;                 -- vystup z D klopneho obvodu po druhe urovni synchronizace signalu RST
+signal din_syn0 : std_logic;                -- vystup z D klopneho obvodu po prvni urovni synchronizace signalu DIN
+signal din_syn : std_logic;                 -- vystup z D klopneho obvodu po druhe urovni synchronizace signalu DIN
 
 begin
+    
+    -- synchronizace vstupu RST pomoci klopnych obvodu typu D (zde pouzity 2)
+    -- prvni klopny obvod typu D - vystupem je signal rst_syn0
+    RST_SYNCHRO_0:  process(CLK) begin
+                        if rising_edge(CLK) then
+                            rst_syn0 <= RST;
+                        end if;
+                    end process;   
+    -- druhy klopny obvod typu D - vystupem je rst_syn, ktery je take vstupem do FSM
+    RST_SYNCHRO:    process(CLK) begin
+                        if rising_edge(CLK) then
+                            rst_syn <= rst_syn0;
+                        end if;
+                    end process;
+                    
+    -- synchronizace vstupu DIN pomoci klopnych obvodu typu D (zde pouzity 2)
+    -- prvni klopny obvod typu D - vystupem je signal din_syn0
+    DIN_SYNCHRO_0:  process(CLK) begin
+                        if rising_edge(CLK) then
+                            din_syn0 <= DIN;
+                        end if;
+                    end process;
+     -- druhy klopny obvod typu D - vystupem je din_syn, ktery je take vstupem do FSM
+    DIN_SYNCHRO:    process(CLK) begin
+                        if rising_edge(CLK) then
+                            din_syn <= din_syn0;
+                        end if;
+                    end process;
+                    
+    -- namapovani signalu na porty FSM
     FSM: entity work.UART_FSM(behavioral)
     port map  (
         CLK           => CLK,              -- hodinovy signal
-        RST           => RST,              -- reset
-        DIN           => DIN,              -- data in
+        RST           => rst_syn,          -- synchronizovany signal reset
+        DIN           => din_syn,          -- synchronizovany signal data in
         CLK_CNT       => clk_cnt,          -- citac hodinovych signalu
         BIT_CNT       => bit_cnt,          -- citac bitu (pouzit pri DATA_READ)
-        STOP_BIT_EN   => stop_bit_en,      -- signalizace, zda je stop bit prijat
+        STOP_BIT_EN   => stop_bit_en,      -- signalizace, zda byl stop bit detekovan
         
-        STOP_BIT_W    => stop_bit_w,       -- signalizace, ze cekame na stop bit
-        CLK_CNT_EN    => clk_cnt_en,       -- signalizace, zda je aktivovan citac hodinoveho signálu
-        DREAD         => dread,             -- signalizace, zda je aktivovan rezim cteni dat
-        DVALID        => dvalid
+        STOP_BIT_W    => stop_bit_w,       -- indikace, ze se ceka na stop bit
+        CLK_CNT_EN    => clk_cnt_en,       -- indikace, zda je aktivovan citac hodinoveho signálu
+        DREAD         => dread,            -- indikace, zda je aktivovan rezim cteni dat
+        DVALID        => dvalid            -- indikace, zda ma byt zapsan DOUT_VLD
     );
     
     process (CLK) begin
-        if rising_edge(CLK) then
-          
+        if (CLK'event) and (CLK = '1') then
             -- pokud neni dread aktivovan, pocitadlo bitu je vynulovano
-            if dread = '0' then
+            if (dread = '0') then
                 bit_cnt <= "0000";
             end if;
-            
-            -- pokud je aktivovan citac hodinoveho signalu, inkrementujeme ho
-            if clk_cnt_en = '1' then
+            -- pokud je aktivovan citac hodinoveho signalu, bude inkrementovan
+            if (clk_cnt_en = '1') then
                 clk_cnt <= clk_cnt + 1;
             else
                 clk_cnt <= "00000";
             end if;
             
             -- pokud jsme ve stavu cteni dat
-            if dread = '1' then
-                if clk_cnt (4) = '1' then
-                    DOUT(to_integer(unsigned(bit_cnt))) <= DIN;
-                    bit_cnt <= bit_cnt + 1;
-                    clk_cnt <= "00000";
-                end if;
-                if clk_cnt = "10000" then
-                    DOUT(to_integer(unsigned(bit_cnt))) <= DIN;
+            if (dread = '1') then
+                if (clk_cnt (4) = '1') then
+                    DOUT(to_integer(unsigned(bit_cnt))) <= din_syn;
                     bit_cnt <= bit_cnt + 1;
                     clk_cnt <= "00000";
                 end if;
             end if;  
             
             -- pokud cekame na stop bit, cekame, nez DIN vysle log.1
-                -- pokud DIN vyslala 1, tedy stop bit, muzeme se prepnout do dalsiho stavu
-            if stop_bit_w = '1' then
-                if DIN = '1' then
+            if (stop_bit_w = '1') then
+                if (din_syn = '1') then
                     stop_bit_en <= '1';
                 else
                     stop_bit_en <= '0';
                 end if;
             end if;
+            
             -- pokud jsme ve stavu DATA_VALID, vysleme DOUT_VLD
-            if dvalid = '1' then
+            if (dvalid = '1') then
                 DOUT_VLD <= '1';
             else
                 DOUT_VLD <= '0';
--- uart_fsm.vhd
+++ uart_fsm.vhd
@@ -1,13 +1,8 @@
 -- uart_fsm.vhd: UART controller - finite state machine
--- Author(s): Lucie Svobodová¡, xsvobo1x
+-- Author(s): Lucie Svobodova, xsvobo1x
 --
 library ieee;
 use ieee.std_logic_1164.all;
-
----- odstranit - jen pro report
-use ieee.std_logic_unsigned.all;
-use ieee.numeric_std.all;
-
 -------------------------------------------------
 -- vstupni a vystupni porty
 entity UART_FSM is
@@ -19,66 +14,97 @@ port(
     BIT_CNT     : in std_logic_vector(3 downto 0);  -- citac bitu (pouzit pri DATA_READ)
     STOP_BIT_EN : in std_logic;                     -- detekce prijeti stop bitu (1 = je nastaven)
     
-    STOP_BIT_W  : out std_logic;                    -- signalizace, ze cekame na stop bit
-    CLK_CNT_EN  : out std_logic;                    -- signalizace, ze je aktivovan citac hodinoveho signalu
-    DREAD       : out std_logic;                    -- signalizace, ze je aktivovan rezim ctení dat
-    DVALID      : out std_logic
+    STOP_BIT_W  : out std_logic;                    -- indikace, ze cekame na stop bit
+    CLK_CNT_EN  : out std_logic;                    -- indikace, ze je aktivovan citac hodinoveho signalu
+    DREAD       : out std_logic;                    -- indikace, ze je aktivovan rezim ctení dat
+    DVALID      : out std_logic                     -- indikace, ze ma byt zapsat DOUT_VLD
     );
 end entity UART_FSM;
 
 -------------------------------------------------
 architecture behavioral of UART_FSM is
 type state_t is (WAIT_FOR_START, WAIT_FOR_FIRST, DATA_READ, WAIT_FOR_STOP, DATA_VALID);
-signal state : state_t := WAIT_FOR_START;
+signal pstate : state_t;  -- soucasny stav (present state)
+signal nstate : state_t;  -- nasledujici stav
+
 begin
-  -- nastaveni vystupu
-  -- pokud jsme ve stavu READ_DATA, nastavime vystupni signal DREAD na 1
-  DREAD <= '1' when state = DATA_READ else '0';
-  -- pokud jsme ve stavu WAIT_FOR_FIRST nebo DATA_READ, vyuzivame clock counter
-  CLK_CNT_EN <= '1' when state = WAIT_FOR_FIRST or state = DATA_READ else '0';
-  -- signalizace cekani na stop bit
-  STOP_BIT_W <= '1' when state = WAIT_FOR_STOP else '0';
-  -- signalizace vyslani DOUT_VLD
-  DVALID <= '1' when state = DATA_VALID else '0';
+  -- Present State Register
+  pstate_reg: process(RST, CLK)
+  begin
+      if (RST = '1') then
+          pstate <= WAIT_FOR_START;
+      elsif (CLK'event) and (CLK = '1') then
+          pstate <= nstate;
+      end if;
+  end process; -- pstate_logic
   
-  process (CLK) begin
-      if rising_edge(CLK) then
-          -- pokud je nastaven vstup RST na log.1, zaciname cekat na START_BIT
-        if RST = '1' then
-            state <= WAIT_FOR_START;
-        -- pokud RST neni nastaven na log.1, prenos uz zacal
-        else
-            case state is
-                -- zacatek prenosu je zahajen nastavenim DIN na log.1 
-                when WAIT_FOR_START =>  if DIN = '0' then
-                                            state <= WAIT_FOR_FIRST;
-                                        end if;
-                --report "FSM: WAIT_FOR_START, clk: " & integer'image(to_integer(unsigned(clk_cnt)));
-                -- po 24 hodinových signalech od prvni detekce start bitu se prepne do stavu cteni dat                       
-                when WAIT_FOR_FIRST =>  if CLK_CNT = "10110" then
-                                            state <= DATA_READ;
-                                        end if;
-                --report "FSM: WAIT_FOR_FIRST, clk: " & integer'image(to_integer(unsigned(clk_cnt)));  
-                -- data jsou ctena do doby, nez je nacteno 8 bitu - citac BIT_CNT                        
-                when DATA_READ      =>  if BIT_CNT = "1000" then
-                                            state <= WAIT_FOR_STOP;
-                                        end if;
-                --report "FSM: DATA_READ, clk: " & integer'image(to_integer(unsigned(clk_cnt)));  
-                --report "FSM: DATA_READ, bit: " & integer'image(to_integer(unsigned(bit_cnt)));
-                
-                -- po nacteni 8 bitu se pocka na nactení stop bitu                           
-                when WAIT_FOR_STOP =>   if STOP_BIT_EN = '1' then
-                                            state <= DATA_VALID;
-                                        end if;
-                --report "FSM: WAIT_FOR_STOP, clk: " & integer'image(to_integer(unsigned(clk_cnt)));  
-                -- po prijetí stop bitu je na dobu jednoho hodinoveho taktu nastaven vystup DOUT_VLD
-                -- po jednom hodinovém taktu se vracime do stavu WAIT_FOR_FIRST  
-                when DATA_VALID     =>  state <= WAIT_FOR_START;
-                --report "FSM: DATA_VALID, clk: " & integer'image(to_integer(unsigned(clk_cnt)));          
-                
-                when others         =>  null;
-            end case;
-        end if;
-    end if;
-end process;
+  -- Next State Logic
+  nstate_logic: process (pstate, DIN, CLK_CNT, BIT_CNT, STOP_BIT_EN)
+  begin
+      case pstate is
+          -- zacatek prenosu je zahajen nastavenim DIN na log.0 
+          when WAIT_FOR_START =>  if (DIN = '0') then
+                                      nstate <= WAIT_FOR_FIRST;
+                                  end if;                       
+          -- cekame, nez je prenesen start bit a nez se dostaneme do poloviny prvniho bitu
+          when WAIT_FOR_FIRST =>  if (CLK_CNT = "10100") then
+                                      nstate <= DATA_READ;
+                                 end if;
+          -- data jsou ctena do doby, nez je nacteno 8 bitu - citac BIT_CNT                        
+          when DATA_READ      =>  if (BIT_CNT = "1000") then
+                                      nstate <= WAIT_FOR_STOP;
+                                  end if;                
+          -- po nacteni 8 bitu se pocka na nactení stop bitu                           
+          when WAIT_FOR_STOP  =>  if (STOP_BIT_EN = '1') then
+                                      nstate <= DATA_VALID;
+                                  end if;
+          -- po prijetí stop bitu je na dobu jednoho hodinoveho taktu nastaven vystup DOUT_VLD
+          -- po jednom hodinovém taktu se vracime do stavu WAIT_FOR_FIRST  
+          when DATA_VALID     =>  nstate <= WAIT_FOR_START;
+          when others         =>  nstate <= WAIT_FOR_FIRST;
+      end case;
+  end process; -- nstate_logic 
+  
+  -- Output Logic - nastaveni vystupu
+  output_logic: process(pstate)
+  begin
+      case pstate is
+          when WAIT_FOR_START =>
+              DREAD <= '0';
+              CLK_CNT_EN <= '0';
+              STOP_BIT_W <= '0';
+              DVALID <= '0';
+              
+          when WAIT_FOR_FIRST =>
+              DREAD <= '0';
+              CLK_CNT_EN <= '1';  -- indikace pracujiciho citace hodinoveho signalu
+              STOP_BIT_W <= '0';
+              DVALID <= '0';
+                    
+          when DATA_READ =>
+              DREAD <= '1';       -- indikace cteni dat
+              CLK_CNT_EN <= '1';  -- indikace pracujiciho citace hodinoveho signalu
+              STOP_BIT_W <= '0';
+              DVALID <= '0';
+                      
+          when WAIT_FOR_STOP =>
+              DREAD <= '0';
+              CLK_CNT_EN <= '0';
+              STOP_BIT_W <= '1';  -- indikace cekani na stop bit
+              DVALID <= '0';
+              
+          when DATA_VALID =>
+              DREAD <= '0';
+              CLK_CNT_EN <= '0';
+              STOP_BIT_W <= '0';
+              DVALID <= '1';      -- indikace, ze ma byt na vystup poslan DOUT_VLD='1'
+              
+          when others =>
+              DREAD <= '0';
+              CLK_CNT_EN <= '0';
+              STOP_BIT_W <= '0';
+              DVALID <= '0';
+      end case;
+  end process; -- output_logic
+  
 end behavioral;
